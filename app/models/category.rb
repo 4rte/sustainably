6859f327b5e91c7d39536d011f62223b9c90641d
Artemij Bussovikov <arte912@gmail.com>
2020-11-24 12:54:58 +0100

    set-up backend user story from home to subcategories page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :sub_categories
   has_many :businesses
+  validates :title, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create! email: 'player@one.com', password: 123456
+User.create! email: 'player@two.com', password: 123456
+User.create! email: 'player@three.com', password: 123456
+
+9.times do |x|
+  seed_category = Category.new title: "Category #{x}"
+  3.times do |y|
+    sub_category = SubCategory.new title: "SubCategory #{y}"
+    sub_category.category = seed_category
+    sub_category.save
+    # p "#{sub_category} created"
+  end
+  seed_category.save
+  # p "#{seed_category} created"
+end
