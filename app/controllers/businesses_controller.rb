38d43c6292b97be40b3f3acc65b368889769ad0c
Artemij Bussovikov <arte912@gmail.com>
2020-11-24 18:10:47 +0100

    routes / DB to be changed with Val tmrw, new seeds, form for adding a business

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,7 +1,30 @@
 class BusinessesController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    raise
-    @businesses = Business.all
+    if params[:sub_category_id].present?
+      @sub_category = SubCategory.find(params[:sub_category_id])
+      @businesses = Business.where(category: @sub_category.category)
+    else
+      @businesses = Business.all
+    end
+  end
+
+  def show
+    @business = Business.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    if current_user.admin?
+      @business = Business.new
+    end
+  end
+
+  def create
+    if current_user.admin?
+      @business = Business.new(business_params)
+      @category = Category.find(params[:category_id])
+      @category = @business.category
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Business.destroy_all
-Category.destroy_all
-SubCategory.destroy_all
-User.destroy_all
 
-User.create! email: 'player@one.com', password: 123456
+# Business.destroy_all
+# Category.destroy_all
+# SubCategory.destroy_all
+# User.destroy_all
+
+User.create! email: 'player@one.com', password: 123456, admin: true
 User.create! email: 'player@two.com', password: 123456
 User.create! email: 'player@three.com', password: 123456
 
-9.times do |x|
-  seed_category = Category.new title: "Category #{x}"
-  3.times do |y|
-    sub_category = SubCategory.new title: "SubCategory #{y}"
-    sub_category.category = seed_category
-    sub_category.save
-    # p "#{sub_category} created"
-  end
-  seed_category.save
-  # p "#{seed_category} created"
-end
+seed_category_1 = Category.new title: "Fashion"
+  sub_category_1 = SubCategory.new title: "Clothes"
+  sub_category_1.category = seed_category_1
+  sub_category_1.save
+    sub_category_2 = SubCategory.new title: "Sportswear"
+    sub_category_2.category = seed_category_1
+    sub_category_2.save
+      sub_category_3 = SubCategory.new title: "Shoes"
+      sub_category_3.category = seed_category_1
+      sub_category_3.save
+        sub_category_4 = SubCategory.new title: "Accessories"
+        sub_category_4.category = seed_category_1
+        sub_category_4.save
+seed_category_1.save
 
-Business.create!(name: "Business 1", category_id: 1)
-Business.create!(name: "Business 2", category_id: 2)
-Business.create!(name: "Business 3", category_id: 3)
+
+seed_category_2 = Category.new title: "Finance"
+seed_category_2.save
+
+seed_category_3 = Category.new title: "Travel"
+seed_category_3.save
+
+seed_category_4 = Category.new title: "Energy"
+seed_category_4.save
+
+seed_category_5 = Category.new title: "Food"
+seed_category_5.save
+
+seed_category_6 = Category.new title: "Home"
+seed_category_6.save
+
+
+# 9.times do |x|
+#   seed_category = Category.new title: "Category #{x}"
+#   3.times do |y|
+#     sub_category = SubCategory.new title: "SubCategory #{y}"
+#     sub_category.category = seed_category
+#     sub_category.save
+#     # p "#{sub_category} created"
+#   end
+#   seed_category.save
+#   Business.create!(name: "Business #{x}", category: seed_category)
+#   # p "#{seed_category} created"
+# end
+
