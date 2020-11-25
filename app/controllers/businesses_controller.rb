9d12ec92430c4825e92f8577473700c0383ea8bc
Frank van Luling <octillerwie@gmail.com>
2020-11-25 14:17:51 +0100

    Fixed most of the backend, just need create for business

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,9 +1,11 @@
 class BusinessesController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    # raise
     if params[:sub_category_id].present?
       @sub_category = SubCategory.find(params[:sub_category_id])
-      @businesses = Business.where(category: @sub_category.category)
+      # raise
+      @businesses = Business.joins(:business_sub_categories).where(business_sub_categories: { sub_category: @sub_category })
     else
       @businesses = Business.all
     end
@@ -15,16 +17,26 @@ class BusinessesController < ApplicationController
   end
 
   def new
+    # raise
     if current_user.admin?
       @business = Business.new
     end
   end
 
   def create
+    # raise
     if current_user.admin?
       @business = Business.new(business_params)
-      @category = Category.find(params[:category_id])
-      @category = @business.category
+      if @business.save
+        raise
+        redirect_to @business, notice: "Tool was successfully added"
+      else
+        render :new
+      end
     end
+  end
+
+  def business_params
+    params.require(:business).permit(:name, :description, :address)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_100130) do
+ActiveRecord::Schema.define(version: 2020_11_25_111638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "business_sub_categories", force: :cascade do |t|
+    t.bigint "business_id", null: false
+    t.bigint "sub_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["business_id"], name: "index_business_sub_categories_on_business_id"
+    t.index ["sub_category_id"], name: "index_business_sub_categories_on_sub_category_id"
+  end
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -23,15 +32,6 @@ ActiveRecord::Schema.define(version: 2020_11_25_100130) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "businesses_sub_categories", force: :cascade do |t|
-    t.bigint "business_id", null: false
-    t.bigint "sub_category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["business_id"], name: "index_businesses_sub_categories_on_business_id"
-    t.index ["sub_category_id"], name: "index_businesses_sub_categories_on_sub_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_100130) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "businesses_sub_categories", "businesses"
-  add_foreign_key "businesses_sub_categories", "sub_categories"
+  add_foreign_key "business_sub_categories", "businesses"
+  add_foreign_key "business_sub_categories", "sub_categories"
   add_foreign_key "reviews", "businesses"
   add_foreign_key "reviews", "users"
   add_foreign_key "sub_categories", "categories"
