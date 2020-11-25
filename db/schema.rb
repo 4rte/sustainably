e97ca4db4ebd2799821d599690d166dddb2f88cb
Marie Chauvois <mrchvsghn@gmail.com>
2020-11-25 16:47:25 +0100

    landing page almost done

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2020_11_25_111638) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "businesses_sub_categories", force: :cascade do |t|
+    t.bigint "business_id", null: false
+    t.bigint "sub_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["business_id"], name: "index_businesses_sub_categories_on_business_id"
+    t.index ["sub_category_id"], name: "index_businesses_sub_categories_on_sub_category_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
@@ -77,6 +86,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_111638) do
 
   add_foreign_key "business_sub_categories", "businesses"
   add_foreign_key "business_sub_categories", "sub_categories"
+  add_foreign_key "businesses_sub_categories", "businesses"
+  add_foreign_key "businesses_sub_categories", "sub_categories"
   add_foreign_key "reviews", "businesses"
   add_foreign_key "reviews", "users"
   add_foreign_key "sub_categories", "categories"
