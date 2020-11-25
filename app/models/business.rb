a878c0578523ab3e667b978b99bd15fd890dbf49
Frank van Luling <octillerwie@gmail.com>
2020-11-25 17:29:33 +0100

    Valerie fixed the create business

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,4 +2,11 @@ class Business < ApplicationRecord
   has_many :reviews
   has_many :business_sub_categories
   has_many :sub_categories, through: :business_sub_categories
+  accepts_nested_attributes_for :sub_categories, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :business_sub_categories, reject_if: :all_blank, allow_destroy: true
+  # belongs_to :sub_category
+
+  def category
+    sub_categories.first.category # the category of the business
+  end
 end
