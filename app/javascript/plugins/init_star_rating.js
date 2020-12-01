695340270d9e0893665e47e16f84f1fd4bea1f9b
Artemij Bussovikov <arte912@gmail.com>
2020-12-01 13:03:23 +0100

    implemented star rating system, styled dropdown & header business show page

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -9,4 +9,19 @@ const initStarRating = () => {
   });
 };
 
+
+const displayStarRating = () => {
+  var ratingValue = document.getElementsByClassName("dropdown-content"), rounded = (ratingValue | 0);
+  var decimal = ratingValue - rounded;
+
+  for (var j = 0; j < rounded ; j++) {
+    $(".rating").append('<i class="fa fa-star" aria-hidden="true"></i>');
+}
+
+  if(decimal){
+    $(".rating").append('<i class="fa fa-star-half" aria-hidden="true"></i>');
+}
+
+};
+
 export { initStarRating };
