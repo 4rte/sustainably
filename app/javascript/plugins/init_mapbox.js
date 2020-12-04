de98d7ea8e1ed23e594520ece22990d800bee76f
Artemij Bussovikov <arte912@gmail.com>
2020-12-04 08:58:15 +0100

    fixed map zoom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,8 +8,9 @@ const buildMap = (mapElement) => {
     style: 'mapbox://styles/4rte/cki8o02td4sy319rx02vye0rt',
     pitch: 60, // pitch in degrees
     // bearing: -60, // bearing in degrees
-    zoom: 15
+    zoom: 13
   });
+  
 };
 
 const addMarkersToMap = (map, markers) => {
@@ -32,8 +33,29 @@ const addMarkersToMap = (map, markers) => {
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat], [marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 13 });
+  map.setCenter([2.355, 48.8566]);
+  var avglng = (markers.map(m => m.lng).reduce((a, b) => a + b, 0)) / markers.length;
+  var avglat = (markers.map(m => m.lat).reduce((a, b) => a + b, 0)) / markers.length;
+  map.flyTo({
+    // These options control the ending camera position: centered at
+    // the target, at zoom level 9, and north up.
+
+    center: [avglng, avglat],
+    zoom: 13,
+    // bearing: 0,
+    // These options control the flight curve, making it move
+    // slowly and zoom out almost completely before starting
+    // to pan.
+    speed: 2, // make the flying slow
+    curve: 2, // change the speed at which it zooms out
+    // This can be any easing function: it takes a number between
+    // 0 and 1 and returns another number between 0 and 1.
+    easing: function (t) {
+    return t;
+    },
+  });
 };
 
 const initMapbox = () => {
